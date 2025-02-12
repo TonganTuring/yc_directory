dceeba6a11d4e397e7e2dd224651edaf6273b39d
Dean Uata <tonganpride@Deans-MacBook-Pro-2.local>
2025-02-12 12:44:27 -0800

    Added css

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <section className="pink_container">
+      <h1 className="heading">pitch your startup connect with entrepreneurs</h1>
+      <p className="sub-heading !max-w-3xl">
+        Y Combinator is a startup accelerator that provides funding,
+        resources, and support to early-stage startups.
+      </p>  
+    </section>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,56 +1,11 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
-const worksans = localFont({
-  src: [
-    {
-      path: "./fonts/WorkSans-Thin.ttf",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraLight.ttf",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Light.ttf", 
-      weight: "300",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Regular.ttf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Medium.ttf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-SemiBold.ttf",
-      weight: "600",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Bold.ttf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-ExtraBold.ttf",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "./fonts/WorkSans-Black.ttf",
-      weight: "900",
-      style: "normal",
-    },
-  ],
-  variable: "--font-worksans",
-});
+import { Work_Sans } from 'next/font/google'
+
+const workSans = Work_Sans({ 
+  subsets: ['latin'],
+  variable: '--font-work-sans',
+})
 
 export const metadata: Metadata = {
   title: "YC Directory",
@@ -59,16 +14,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html lang="en">
-      <body
-        className={`${worksans.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="en" className={workSans.className}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
