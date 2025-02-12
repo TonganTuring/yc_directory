2c7820891ecbdf68f2194c75a0c3379b01891c72
Dean Uata <tonganpride@Deans-MacBook-Pro-2.local>
2025-02-12 13:55:02 -0800

    implemented startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,18 +1,49 @@
 import SearchForm from "@/components/SearchForm";
 import Image from "next/image";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {searchParams: {query: string}}) {
 
   const query = (await searchParams.query);
 
+  const posts = [{
+    _createdAt: new Date(),
+    views: 100,
+    author: { _id: 1, name: 'John Doe'},
+    _id:1,
+    description: 'This is a description',
+    image: 'https://miro.medium.com/v2/resize:fit:1400/0*7VyEZgzwUhQMeBqb',
+    category: 'Robots',
+    title: 'We Robots'
+  }]
+
   return (
-    <section className="pink_container">
-      <h1 className="heading">pitch your startup connect with entrepreneurs</h1>
-      <p className="sub-heading !max-w-3xl">
-        Y Combinator is a startup accelerator that provides funding,
-        resources, and support to early-stage startups.
-      </p>  
-      <SearchForm query={query} />
-    </section>
+    <main>
+      <section className="pink_container">
+        <h1 className="heading">pitch your startup connect with entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Y Combinator is a startup accelerator that provides funding,
+          resources, and support to early-stage startups.
+        </p>  
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startups found</p>
+          )
+        }
+        </ul>
+      </section>
+    </main>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  dangerouslyAllowSVG: true,
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
