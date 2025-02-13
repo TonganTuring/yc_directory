7841142e62fa8520ca51049c40fed5e6b129d1cc
Dean Uata <tonganpride@dhcp-10-102-11-82.wireless.northwestern.private>
2025-02-12 18:09:49 -0800

    added real-time caching

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,15 @@
 import SearchForm from "@/components/SearchForm";
 import Image from "next/image";
 import StartupCard from "@/components/StartupCard";
-import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 
 export default async function Home({searchParams}: {searchParams: {query: string}}) {
 
   const query = (await searchParams).query;
 
-  const posts = await client.fetch(STARTUPS_QUERY);
+  // const posts = await client.fetch(STARTUPS_QUERY);
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
 
   console.log(JSON.stringify(posts, null, 2));
 
@@ -50,6 +51,8 @@ export default async function Home({searchParams}: {searchParams: {query: string
         }
         </ul>
       </section>
+
+      <SanityLive />
     </main>
   );
 }
